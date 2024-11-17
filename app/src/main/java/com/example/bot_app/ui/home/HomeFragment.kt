70c3d274dd02433e1f6f23920eb2bc14d173fa9e
GitAshoMon <monteagudoace@email.com>
2024-11-17 11:08:08 +0800

    commit

--- app/src/main/java/com/example/bot_app/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/bot_app/ui/home/HomeFragment.kt
@@ -1,7 +1,8 @@
 package com.example.bot_app.ui.home
 
-import android.R.attr.button
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,9 +12,16 @@ import android.widget.EditText
 import android.widget.Spinner
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import com.example.bot_app.R
 import com.example.bot_app.databinding.FragmentHomeBinding
+import kotlinx.coroutines.launch
+import java.io.BufferedInputStream
+import java.io.DataInputStream
+import java.io.IOException
+import java.io.PrintWriter
 import java.net.Socket
 
 
@@ -31,7 +39,7 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+            ViewModelProvider(this)[HomeViewModel::class.java]
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
@@ -81,7 +89,7 @@ class HomeFragment : Fragment() {
 
         sendbtn.setOnClickListener(View.OnClickListener {
             // Code here executes on main thread after user presses button
-            sendData()
+            sendData(senderfield)
         })
         return root
     }
@@ -91,9 +99,64 @@ class HomeFragment : Fragment() {
         _binding = null
     }
 
-    fun sendData(){
-        val client = Socket("127.0.0.1", 9999)
-        client.outputStream.write("Hello from the client!".toByteArray())
-        client.close()
+    private fun sendData(pop: EditText){
+//        val client = Socket("127.0.0.1", 9999)
+//        client.outputStream.write("Hello from the client!".toByteArray())
+//        client.close()
+        pop.setText("hello")
+        val bgt = BackGroundTask()
+        bgt.viewModelScope
     }
+
+    class BackGroundTask: ViewModel ()
+    {
+        val h = Handler()
+        private var s: Socket? = null
+        private var writer: PrintWriter? = null
+        private var reader: DataInputStream? = null
+        private var i: Int = 0
+        private var send: String? = null
+        private var get: String? = null
+        init {
+            viewModelScope.launch {
+
+                doInBackground()
+            }
+        }
+        private fun doInBackground(vararg voids: String?): Void? {
+            Log.i("i", "hello");
+            try {
+                if (s == null) {
+                    //change it to your IP
+                    s = Socket("192.168.137.94", 6000)
+                    writer = PrintWriter(s!!.getOutputStream())
+                    reader = DataInputStream(
+                        BufferedInputStream(s!!.getInputStream())
+                    )
+                    Log.i("i", "CONNECTED")
+                }
+                if ((i % 5) == 0) {
+                    send = "me:fifth"
+                    get = "me:fifth"
+                } else {
+                    send = "me:" + i.toString()
+                    get = "me:" + i.toString()
+                }
+                if (i > 10) {
+                    s!!.close()
+                }
+                writer?.write(send)
+                writer?.flush()
+                //                writer.close();
+                i = i + 1
+                h.post {
+                    //                        Log.i("i", "lol");
+                }
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+            return null
+        }
+    }
+
 }
